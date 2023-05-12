a66159f381a6da4e88c9c2e712798c865fbec90f
PedroPabloGuzmanMayen <ppguzmanmayen@gmail.com>
2023-05-12 12:38:31 -0600

    Methods

--- src/GraphMatrix.java
+++ src/GraphMatrix.java
@@ -1,4 +1,44 @@
 
-public class GraphMatrix {
+public abstract class GraphMatrix implements IGraph {
 
+	@Override
+	public <T> void addVertex(Vertex<T> v) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public <T> void addEdge(T value, Edge<T> e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void vertexNumber() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public <T> void deleteVertex(Vertex<T> v) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public <T> void deleteEdge(Edge<T> e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public <T> void Traverse(T value) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+	public void calculateMatrix() {
+		
+	}
 }
--- src/IGraph.java
+++ src/IGraph.java
@@ -1,4 +1,12 @@
 
 public interface IGraph {
+	
+	<T> void addVertex(Vertex<T> v);
+	<T>void addEdge(T value, Edge<T> e);
+	void vertexNumber();
+	<T> void deleteVertex(Vertex<T> v);
+	<T> void deleteEdge(Edge<T> e);
+	<T> void Traverse(T value);
+	<T> void searchVertex(T value);
 
 }
